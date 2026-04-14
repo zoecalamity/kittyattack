b80b6377587c97a893c204e7ad63624195d1f93d
Zoe Snell <zoe.calamity@gmail.com>
2026-04-14 18:33:29 -0500

    imported assets and loaded them into the program

--- src/project.py
+++ src/project.py
@@ -3,8 +3,22 @@ import random
 import math 
 from pygame import mixer 
 
-pygame.init()
 
+#screen size
 screen_width = 800 
 screen_height = 600 
 screen = pygame.display.set_mode((screen_width, screen_height))
+no_of_invaders = 8 
+
+def init_game(): 
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    pygame.display.set_caption("Kitty Attack!")
+
+def load_assets():
+    player_image = pygame.image.load('data/spaceman.png')
+    fish_image = pygame.image.load('data/fishbullet.png')
+    cat_image = [pygame.image.load('data/aliencat.png') for _ in range(no_of_invaders)]
+    return player_image, fish_image, cat_image
+
+
